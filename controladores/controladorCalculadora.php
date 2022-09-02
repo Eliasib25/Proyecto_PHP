65b4a96dd4b67b76b68dbf2d201d5e0db7916cb2
ELIASIB BENÍTEZ MERCADO <1104008652@ACDCECAR.LOCAL>
2022-09-02 08:48:52 -0500

    POO

--- controladores/controladorCalculadora.php
+++ controladores/controladorCalculadora.php
@@ -1,7 +1,19 @@
 <?php
 
 class ControladorCalculadora {
-    function sumar($operadorUno, $operadorDos){
+
+    private $operadorUno;
+    private $operadorDos;
+
+    function __construct($operadorUno='', $operadorDos=''){
+    $this-> $operadorUno = $operadorUno;
+    $this-> $operadorDos = $operadorDos;
+
+    }
+
+    function sumar($operadorUno='', $operadorDos=''){
+        $operadorUno= empty($operadorUno) ? $this->operadorUno : $operadorUno;
+        $operadorDos= empty($operadorDos) ? $this->operadorDos : $operadorDos;
         return $operadorUno + $operadorDos;
     }
 
